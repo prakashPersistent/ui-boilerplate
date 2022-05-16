c6253421f2cff5a19a9cd7d37f9648436e8498c0
Hardik Godhani <hardik.godhani@lucentinnovation.co.in>
2022-05-16 19:42:27 +0530

    typography ui

--- src/App.js
+++ src/App.js
@@ -1,50 +1,154 @@
+import './scss/_typography-section.scss';
 import './scss/app.scss';
 
 function App() {
   return (
-    
+
     <>
-      <div className='container'>
-        <h1>H1- Hello world</h1>
-        <h2>H2 - Hello world</h2>
-        <p>P tag</p>
-        <button className='button primary-button'>Primary button</button>
-        <br />
-        <br />
-        <button className='button secondary-button'>Primary button</button>
-        <br/><br/>
-        <a href='#'>Link</a>
-        <div class="field">        
-        <input type="email" className="field-input primary-input"  placeholder="Email" />
-        <label>
-          Email
-        </label>
-      </div>
+      <div className='typography-section'>
+        <div className='container'>
+          <div className='typography-box'>
+            <h3 className='typography-title'>Headers</h3>
+            <div className='typography-wrap row'>
+              <div className='grid-md-6'>
+                <h1 className='heading heading-light'>H1 HEADER IVYMODE LIGHT</h1>
+                <code>&lt;h1 class="heading heading-light"&gt;&lt;/h1&gt;</code>
+              </div>
+              <div className='grid-md-6'>
+                <h1 className='heading'>H1 HEADER IVYMODE REGULAR</h1>
+                <code>&lt;h1 class="heading"&gt;&lt;/h1&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h2 className='heading heading-light'>H2 HEADER IVYMODE LIGHT</h2>
+                <code>&lt;h2 class="heading heading-light"&gt;&lt;/h2&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h2 className='heading'>H2 HEADER IVYMODE REGULAR</h2>
+                <code>&lt;h2 class="heading"&gt;&lt;/h2&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h3 className='heading heading-light'>H3 HEADER IVYMODE LIGHT</h3>
+                <code>&lt;h3 class="heading heading-light"&gt;&lt;/h3&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h3 className='heading'>H3 HEADER IVYMODE REGULAR</h3>
+                <code>&lt;h3 class="heading"&gt;&lt;/h3&gt;</code>
+              </div>
+            </div>
+          </div>
 
-        <div class="product-items quantity-wrapper quantity-wrapper-js">
-  <label class="form-label">
-    Quantity
-  </label>
-  <div class="quantity-box">
-    <button class="quantity-button-js quantity-button no-js-hidden" name="minus" type="button">
+          <div className='typography-box'>
+            <h3 className='typography-title'>Sub-Headers</h3>
+            <div className='typography-wrap row'>
+              <div className='grid-md-6'>
+                <h3 className='italic'>H3 Sub-header Italic</h3>
+                <code>&lt;h3 class="italic"&gt;&lt;/h3&gt;</code>
+              </div>
+              <div className='grid-md-6'>
+                <h3 className='light'>H3  Sub-header Light</h3>
+                <code>&lt;h3 class="light"&gt;&lt;/h3&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h4 className='italic'>H4 Sub-header Italic</h4>
+                <code>&lt;h4 class="italic"&gt;&lt;/h4&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h4 className='light'>H4 Sub-header Light</h4>
+                <code>&lt;h4 class="light"&gt;&lt;/h4&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h5 className='italic'>H5 Sub-header Italic</h5>
+                <code>&lt;h5 class="italic"&gt;&lt;/h5&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h5 className='light'>H5 Sub-header Light</h5>
+                <code>&lt;h5 class="light"&gt;&lt;/h5&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h6 className='italic'>H6 Sub-header Italic</h6>
+                <code>&lt;h6 class="italic"&gt;&lt;/h6&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h6 className='light'>H6 Sub-header Light</h6>
+                <code>&lt;h6 class="light"&gt;&lt;/h6&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <h6 className='italic bold'>H6 Sub-header Italic Mobile</h6>
+                <code>&lt;h6 class="italic bold"&gt;&lt;/h6&gt;</code>
+              </div>
+            </div>
+          </div>
 
-  
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" role="presentation" class="icon icon-minus" fill="none" viewBox="0 0 10 2">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M.5 1C.5.7.7.5 1 .5h8a.5.5 0 110 1H1A.5.5 0 01.5 1z" fill="currentColor">
-    </path></svg>
-
-  </button>
-    <input className="quantity-input quantity-input-js" type="number" name="quantity" min="1" value="1" />
-    <button className="quantity-button-js quantity-button no-js-hidden" name="plus" type="button">
-
-  
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" role="presentation" class="icon icon-plus" fill="none" viewBox="0 0 10 10">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M1 4.51a.5.5 0 000 1h3.5l.01 3.5a.5.5 0 001-.01V5.5l3.5-.01a.5.5 0 00-.01-1H5.5L5.49.99a.5.5 0 00-1 .01v3.5l-3.5.01H1z" fill="currentColor">
-    </path></svg>
-
-  </button>
-  </div>
-</div>
+          <div className='typography-box'>
+            <h3 className='typography-title'>Body</h3>
+            <div className='typography-wrap row'>
+              <div className='grid-md-6'>
+                <p className='text-xx-lg'>XX-Large Body</p>
+                <code>&lt;p class="text-xx-lg"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6'>
+                <p className='text-xx-lg light'>XX-Large Body Light</p>
+                <code>&lt;p class="text-xx-lg light"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-x-lg'>X-Large Body</p>
+                <code>&lt;p class="text-xx-lg"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-x-lg light'>X-Large Body Light</p>
+                <code>&lt;p class="text-xx-lg light"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-4 mt-2'>
+                <p className='text-lg'>Large Body</p>
+                <code>&lt;p class="text-lg"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-lg light'>Large Body Light</p>
+                <code>&lt;p class="text-xx-lg light"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-lg italic'>Large Body Italic</p>
+                <code>&lt;p class="text-xx-lg italic"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-lg light italic'>Large Body Light Italic</p>
+                <code>&lt;p class="text-xx-lg light italic"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-4 mt-2'>
+                <p className='text-md'>Medium Body</p>
+                <code>&lt;p class="text-md"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-md light'>Medium Body Light</p>
+                <code>&lt;p class="text-md light"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-4 mt-2'>
+                <p className='text-sm'>Small Body</p>
+                <code>&lt;p class="text-sm"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-sm italic'>Small Italic Body</p>
+                <code>&lt;p class="text-sm italic"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-4 mt-2'>
+                <p className='text-x-sm'>X-Small Body</p>
+                <code>&lt;p class="text-x-sm"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-x-sm light'>X-Small Body Light</p>
+                <code>&lt;p class="text-x-sm light"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-4 mt-2'>
+                <p className='text-xx-sm'>XX-Small Body</p>
+                <code>&lt;p class="text-xx-sm"&gt;&lt;/p&gt;</code>
+              </div>
+              <div className='grid-md-6 mt-2'>
+                <p className='text-xx-sm light'>XX-Small Body Light</p>
+                <code>&lt;p class="text-xx-sm light"&gt;&lt;/p&gt;</code>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
