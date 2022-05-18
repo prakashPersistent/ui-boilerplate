d0c8c2e3173812ddc79e5d5b7e690b2ea38b7e31
prakashPersistent <prakash_prabhakar@persistent.com>
2022-05-18 18:24:47 +0530

    minor change in accordion

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -4,13 +4,24 @@ const Accordion = ({PlusIcon,data}) => {
 
     const [isOpen,setIsOpen] = useState(false);
     const [active,setActive] = useState();
+   
+    const handleClick = (i)=>{
 
+        if(i === active ){
+            setIsOpen(!isOpen);
+        }else{
+            setIsOpen(true);
+            setActive(i)
+        }
+    }
+
+    
     return (
         <div className='typography-wrap row'>
               <div className='grid-md-12'>
                 <div className='custom-accordion'>
                     {data?.map((ele,i)=>(
-                        <div className={`accordion-item ${(isOpen && ele.title === active) && "open"}`} onClick={()=>{setIsOpen(!isOpen);setActive(ele.title)}}>
+                        <div key={i} className={`accordion-item ${(isOpen && i === active) && "open"}`} onClick={()=>handleClick(i)}>
                         <div className='accordion-head'>{ele?.title} <img src={PlusIcon}></img></div>
                         <div className='accordion-body'>
                         {ele?.body}
